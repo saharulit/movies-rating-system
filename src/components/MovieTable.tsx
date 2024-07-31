40786766e5c32979df8acfde734fd16ba40fa187
Sahar Bracha <saharulit@gmail.com>
2024-07-31 10:55:20 +0300

    fix table

--- src/components/MovieTable.tsx
+++ src/components/MovieTable.tsx
@@ -1,20 +1,41 @@
 import React from 'react';
-import { Movie } from '../types';
-import DataTable from 'react-data-table-component';
-import { TABLE_COLUMNS } from '../services/consts';
+import DataTable, { TableColumn } from 'react-data-table-component';
+import { Movie } from '../services/movie';
 
+interface MovieTable extends Movie {
+  totalVotes: number;
+  lastUpdated: string;
+}
 interface MovieTableProps {
-  movies: Movie[];
+  movies: MovieTable[];
 }
 
 const MovieTable: React.FC<MovieTableProps> = ({ movies }) => {
+  const columns: TableColumn<MovieTable>[] = [
+    {
+      name: 'Movie ID',
+      selector: (row: MovieTable) => row.id,
+      sortable: true,
+    },
+    {
+      name: 'Movie description',
+      selector: (row: MovieTable) => row.description,
+      sortable: true,
+    },
+    {
+      name: 'Total votes',
+      selector: (row: MovieTable) => row.totalVotes,
+      sortable: true,
+    },
+    {
+      name: 'Last updated time',
+      selector: (row: MovieTable) => row.lastUpdated,
+      sortable: true,
+    },
+  ];
+
   return (
-    <DataTable
-      title="Movie List"
-      columns={TABLE_COLUMNS}
-      data={movies}
-      pagination
-    />
+    <DataTable title="Movie List" columns={columns} data={movies} pagination />
   );
 };
 
--- src/types.ts
+++ src/types.ts
@@ -2,11 +2,3 @@ export enum ConnectionStatus {
   Connected = 'connected',
   Disconnected = 'disconnected',
 }
-
-export interface Movie {
-  id: number;
-  description: string;
-  totalVotes: number;
-  lastUpdated: string;
-  // positionChange: 'up' | 'down' | 'same';
-}
